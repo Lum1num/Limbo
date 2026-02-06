5ce47a310e5e5453518a2b71c7750d174f9a6481
Luminum <kettawastaken@outlook.com>
2026-02-06 00:14:05 -0800

    Create placedetectorscript.lua

---
+++ source/placedetectorscript.lua
@@ -0,0 +1,10 @@
+function universal()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Lum1num/Limbo/refs/heads/main/source/places/universal.lua', true))()
+end
+function place()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Lum1num/Limbo/refs/heads/main/source/places/'..tostring(game.PlaceId)..'.lua', true))()
+end
+local suc, _ = pcall(place)
+if not suc then
+    pcall(universal)
+end

b34282dc17c28e19bbfc3b00adae6a7cf924615b
Luminum <kettawastaken@outlook.com>
2026-02-06 20:43:10 -0800

    Update 7205641391.lua

--- source/places/7205641391.lua
+++ source/places/7205641391.lua
@@ -1,1 +1,9 @@
-print('???')
+if not isfolder("limbo") then return end
+local Library = loadstring(readfile("limbo/MainGUILoader.lua"))()
+local limbo = shared.limbo
+
+limbo.Window:AddLeftGroupboxWithDescription(
+    limbo.main,
+    "Box",
+    "new" 
+)
